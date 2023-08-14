7898c8b7a7ed5e72b093779fcd560dbebb1ca813
tasesmuemils <emils.bisenieks93@gmail.com>
2023-08-14 23:33:31 +0300

    Remove dark mode error

--- src/app/Switcher.jsx
+++ src/app/Switcher.jsx
@@ -1,23 +1,28 @@
 "use client";
 
 import React, { useState } from "react";
-import useDarkSide from "./useDarkSide";
+// import useDarkSide from "./useDarkSide";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 export default function Switcher() {
-	const [colorTheme, setTheme] = useDarkSide();
-	const [darkSide, setDarkSide] = useState(colorTheme === "light" ? true : false);
+  // const [colorTheme, setTheme] = useDarkSide();
+  // const [darkSide, setDarkSide] = useState(colorTheme === "light" ? true : false);
 
-	const toggleDarkMode = (checked) => {
-		setTheme(colorTheme);
-		setDarkSide(checked);
-	};
+  // const toggleDarkMode = (checked) => {
+  // 	setTheme(colorTheme);
+  // 	setDarkSide(checked);
+  // };
 
-	return (
-		<>
-			<div className="m-2">
-				<DarkModeSwitch sunColor="#64748b" checked={darkSide} onChange={toggleDarkMode} size={24} />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="m-2">
+        <DarkModeSwitch
+          sunColor="#64748b"
+          // checked={darkSide}
+          // onChange={toggleDarkMode}
+          size={24}
+        />
+      </div>
+    </>
+  );
 }

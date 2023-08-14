74f4b7971b1583438e86148d9b5286129b935cae
tasesmuemils <emils.bisenieks93@gmail.com>
2023-08-15 01:10:47 +0300

    PWA edit 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,9 +17,28 @@ export default function RootLayout({ children }) {
       className="bg-white transition-all duration-500 dark:bg-slate-800"
     >
       <head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <meta name="title" content="Zoles punkti" />
+        <meta name="Zoles punkti" content="Zoles punkti" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Zoles punkti" />
+        <meta
+          name="description"
+          content="Neskrien pēc lapas un pildspalvas, skaiti 'Zoles' punktus šeit"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body
         initial={{ opacity: 0 }}

a82d1e082c2a6383cb18e5972393a0a4377471db
tasesmuemils <emils.bisenieks93@gmail.com>
2023-08-17 11:36:51 +0300

    PWA edit 3

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-module.exports = withPWA(nextConfig);
-
-// module.exports = nextConfig
+module.exports = withPWA({
+  // Your Next.js config
+});
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navigation from "./Navigation";
-import NewNavigation from "./NewNavigation";
 
 const inter = Inter({ subsets: ["latin"] });
 

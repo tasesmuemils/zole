d2d906158b9c22f54aa2f10af3b91ff27c19440e
tasesmuemils <emils.bisenieks93@gmail.com>
2023-08-17 12:23:39 +0300

    PWA edit 5

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+
+  exclude: [
+    // add buildExcludes here
+    ({ asset, compilation }) => {
+      if (
+        asset.name.startsWith("server/") ||
+        asset.name.match(
+          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
+        )
+      ) {
+        return true;
+      }
+      if (isDev && !asset.name.startsWith("static/runtime/")) {
+        return true;
+      }
+      return false;
+    },
+  ],
 });
 
-module.exports = withPWA({
-  // Your Next.js config
-});
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);

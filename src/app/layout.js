3a62ba2301439e5da14c5217049e646b7db31c1b
tasesmuemils <emils.bisenieks93@gmail.com>
2023-08-17 12:50:31 +0300

    PWA edit 6

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,11 @@ export const metadata = {
     default: APP_DEFAULT_TITLE,
     template: APP_TITLE_TEMPLATE,
   },
+  // icons: {
+  //   icon: ["/favicon.ico?v=4"],
+  //   apple: ["/apple-touch-icon.png?v=4"],
+  //   shortcut: ["/apple-touch-icon.png"],
+  // },
   description: APP_DESCRIPTION,
   manifest: "/manifest.json",
   themeColor: "#FFFFFF",
@@ -25,28 +30,17 @@ export const metadata = {
     capable: true,
     statusBarStyle: "default",
     title: APP_DEFAULT_TITLE,
-    // startUpImage: [],
+    startupImage: [
+      "/apple-touch-icon.png",
+      {
+        url: "/apple-touch-icon.png",
+        media: "(device-width: 768px) and (device-height: 1024px)",
+      },
+    ],
   },
   formatDetection: {
     telephone: false,
   },
-  // openGraph: {
-  //   type: "website",
-  //   siteName: APP_NAME,
-  //   title: {
-  //     default: APP_DEFAULT_TITLE,
-  //     template: APP_TITLE_TEMPLATE,
-  //   },
-  //   description: APP_DESCRIPTION,
-  // },
-  // twitter: {
-  //   card: "summary",
-  //   title: {
-  //     default: APP_DEFAULT_TITLE,
-  //     template: APP_TITLE_TEMPLATE,
-  //   },
-  //   description: APP_DESCRIPTION,
-  // },
 };
 
 export default function RootLayout({ children }) {
@@ -55,30 +49,21 @@ export default function RootLayout({ children }) {
       lang="en"
       className="bg-white transition-all duration-500 dark:bg-slate-800"
     >
-      {/* <head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          type="image/<generated>"
+          // sizes="<generated>"
         />
-        <meta name="title" content="Zoles punkti" />
-        <meta name="Zoles punkti" content="Zoles punkti" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Zoles punkti" />
-        <meta
-          name="description"
-          content="Neskrien pēc lapas un pildspalvas, skaiti 'Zoles' punktus šeit"
+        <link
+          rel="apple-touch-icon"
+          href="apple-touch-icon.png"
+          type="image/png"
+          // sizes="<generated>"
         />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
-
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </head> */}
+      </head>
       <body
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}

c46123419759081be89a75ed5a38dfc0f1c1219e
tasesmuemils <emils.bisenieks93@gmail.com>
2023-09-22 01:27:45 +0300

    SEO button edit player count

--- src/app/PlayersCount.jsx
+++ src/app/PlayersCount.jsx
@@ -7,7 +7,7 @@ export default function PlayersCount(props) {
   return (
     <div>
       <button
-        aria-label="palyer-count"
+        aria-label={`palyer-count-${props.number}`}
         className="grow rounded-lg text-base leading-6 font-semibold px-5 py-1 m-2 ring-2 ring-inset hover:bg-cyan-500 dark:hover:bg-cyan-500 hover:ring-cyan-500 hover:text-slate-50 ring-slate-500 text-slate-500 transition-all duration-500 dark:text-slate-100 dark:ring-inset dark:bg-slate-500"
         onClick={handleClick}
       >
